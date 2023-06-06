fa39cb05afccf4e9d62064aa6e166786e769f3fa
Leo Winters <leopwinters@protonmail.com>
2023-06-06 23:06:48 +0200

    Add the first page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,44 @@
-<h1>Welkom</h1>
+<svelte:head>
+	<title>Strijd om aandacht</title>
+</svelte:head>
+
+<article>
+	<h1>Onze aandacht wordt gestolen</h1>
+
+	<address>
+		Door Leo Winters
+		<a rel="author" href="mailto:leopwinters@protonmail.com">leopwinters@protonmail.com</a>
+	</address>
+	<time datetime="2023-06-06" title="6 juni 2023">Op 6 juni 2023</time>
+
+	<p>
+		Ik heb veel tijd besteed op websites als YouTube, Netflix en Reddit. Tijd waarin ik zelf niets
+		doe. Het zijn "activiteiten" die me leeg doen voelen. Dat wil ik niet meer.
+	</p>
+
+	<p>
+		Maar stoppen blijkt lastig. Deze diensten hebben een aanzuigende werking. En dat is niet
+		toevallig. Zo zijn ze ontworpen.
+	</p>
+
+	<p>
+		Want bedrijven strijden om onze aandacht, om die te verkopen aan de hoogste bieder. En ze worden
+		er steeds beter in.
+	</p>
+
+	<p>We moeten ons dus wapenen. We moeten slim zijn.</p>
+
+	<p>En voor mij is deze website het begin. Hier begint mijn actie. Hier maak ik.</p>
+
+	<p>
+		Doe met me mee. Maak iets, op wat voor manier dan ook. Pak de macht terug. En laten we het
+		delen. Stuur me een mailtje :)
+	</p>
+</article>
+
+<style>
+	article {
+		max-width: 60ch;
+		margin: auto;
+	}
+</style>
